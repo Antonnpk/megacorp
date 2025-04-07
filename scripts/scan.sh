d04686feb8cbaa968337020dda13a70c362e8c7d
Anthony <115882066+Antonnpk@users.noreply.github.com>
2025-04-07 15:44:53 +0200

    3: Third Change

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -5,3 +5,4 @@ echo "\n===== SCANNING FOR SOCIAL SECURITY NUMBERS ====="
 grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
 echo "====== SOCIAL SECURITY SCAN COMPLETE ======"
 echo "phone numbers"
+Third Change
